f51b1f38cc9271f05f36177d6285f6936b9b54a2
sahas <munamalasahas@gmail.com>
2021-01-08 17:57:31 -0800

    recreate repository with jsx

--- src/Components/HouseModel.tsx
+++
@@ -1,6 +0,0 @@
-import { useGLTF } from 'drei';
-
-export default function HouseModel() {
-  const gltf = useGLTF('/LittlestTokyo.glb');
-  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
-}
