1dca58b4deef655389f6db5db201f96b901810c1
sahas <munamalasahas@gmail.com>
2021-01-14 21:44:59 -0800

    remove secret key from git history and settings.py

--- backend/homolo/settings.py
+++ backend/homolo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'scoy^$hpz2_f28w3h-es=1$x@bs334bx(1(g3^o=1s1_!1d7q3'
+SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
