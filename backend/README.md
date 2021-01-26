46d83bc5857272a53affa11fb9e1c1636fe229de
sahas <munamalasahas@gmail.com>
2021-01-25 16:25:31 -0800

    add streaming from webcam to rest api

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# Homolo App
+
+Web application to view all security cameras in and around your home from the browser. Uses threejs to load a model of your home, and loads live video feeds, stores movement detected from cameras, and runs the video through an object detection algorithm to find people.
+
+
--- backend/stream/views.py
+++ backend/stream/views.py
@@ -1,19 +1,21 @@
 from django.http import Http404
+from django.http.response import StreamingHttpResponse
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from camera.models import Camera, Movement
-from camera.serializers import CameraSerializer
-
 from stream.camera import LiveWebCam, FrameDropException, FrameConversionException
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 class CameraView(APIView):
     """
     Create HTTPStream for ip camera frames
     """
     def get(self, request, pk, format=None):
-        cameras = Camera.objects.filter(cam_id=pk)
-        serializer = CameraSerializer(cameras, many=True)
-        return Response(serializer.data)
+        return StreamingHttpResponse(gen(LiveWebCam()), content_type='multipart/x-mixed-replace; boundary=frame')
